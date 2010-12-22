1b97c751e6a566417714b9cc3588d7f9bdeecdfc
Andrea Cimitan <andrea.cimitan@canonical.com>
2010-12-22 03:28:46 +0100

    don't theme scrollbar steppers, for the moment

--- unico/unico-engine.c
+++ unico/unico-engine.c
@@ -59,7 +59,12 @@ unico_engine_render_background (GtkThemingEngine *engine,
 
   unico_lookup_functions (UNICO_ENGINE (engine), &style_functions);
 
-  if (gtk_theming_engine_has_class (engine, GTK_STYLE_CLASS_BUTTON))
+  if (gtk_theming_engine_has_class (engine, GTK_STYLE_CLASS_BUTTON) &&
+      gtk_theming_engine_has_class (engine, GTK_STYLE_CLASS_SCROLLBAR))
+    {
+      GTK_THEMING_ENGINE_CLASS (unico_engine_parent_class)->render_background (engine, cr, x, y, width, height);
+    }
+  else if (gtk_theming_engine_has_class (engine, GTK_STYLE_CLASS_BUTTON))
     {
       UnicoButtonParameters *button;
 
@@ -135,7 +140,12 @@ unico_engine_render_frame (GtkThemingEngine *engine,
 
   unico_lookup_functions (UNICO_ENGINE (engine), &style_functions);
 
-  if (gtk_theming_engine_has_class (engine, GTK_STYLE_CLASS_BUTTON))
+  if (gtk_theming_engine_has_class (engine, GTK_STYLE_CLASS_BUTTON) &&
+      gtk_theming_engine_has_class (engine, GTK_STYLE_CLASS_SCROLLBAR))
+    {
+      GTK_THEMING_ENGINE_CLASS (unico_engine_parent_class)->render_frame (engine, cr, x, y, width, height);
+    }
+  else if (gtk_theming_engine_has_class (engine, GTK_STYLE_CLASS_BUTTON))
     {
       UnicoButtonParameters *button;
 
